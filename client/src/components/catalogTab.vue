0e03d68973c7b0424872a2a789014d8c35d06ce3
vant110 <vant110@yandex.ru>
2021-05-22 03:49:20 +0400

    Add navigation logic

--- client/src/components/catalogTab.vue
+++ client/src/components/catalogTab.vue
@@ -1,50 +1,81 @@
 <template>
-<div class="layout__row layout__row_body">
-			<div class="container">
-				<div class="grid">
-					<!--TODO обработчик-->
-						<div class="pet"
-						v-for="(pet, index) in pets"
-						v-bind:item="pet"
-						v-bind:index="index"
-						v-bind:key="pet._id"
-						>
-							<a class="cards-a-with-img" @click="$emit('cardPressed', pet)" href="#">
-								<img :src="require(`../assets/images/${pet.imgPath}`)">
-							</a>
-							<div class="box cards-div-name">
-								<a class="cards-name" href="#">{{pet.name}}</a>
-							</div>
-						</div>
-					<p class="error" v-if="error">{{ error }}</p>
+	<div class="layout__row layout__row_body">
+		<div class="container">
+			<div class="grid">
+				<div class="pet"
+				v-for="(pet, index) in filteredPets"
+				:item="pet"
+				:index="index"
+				:key="pet._id"
+				>
+					<a class="cards-a-with-img" @click="cardPressed(pet)" href="#">
+						<img :src="getImgPath(pet._id)">
+					</a>
+					<div class="box cards-div-name">
+						<a class="cards-name" @click="cardPressed(pet)" href="#">{{ pet.name }}</a>
+					</div>
 				</div>
+				<p class="error" v-if="error">{{ error }}</p>
 			</div>
 		</div>
+	</div>
 </template>
 
 <script>
-import PetsImport from '../PetsImport.js'
+import PetsImport from '../api/PetsImport.js'
 
 export default {
-  name: 'PetsImport',
-  data(){
-    return {
-		pets: [],
-		error: ''
+	name: 'catalogTab',
+	props: {
+		kindOfPets: {
+			type: String,
+			default: ''
+		}, 
+		breedOfPets: {
+			type: String,
+			default: ''
+		}
+	},
+	data() {
+		return {
+			pets: [],
+			error: ''
+		}
+	},
+	methods: {
+		cardPressed(pet) {
+			this.$emit('cardPressed', pet)
+		},
+		getImgPath(id) {
+			var kind
+			switch (this.kindOfPets) {
+				case 'Кошки': 
+					kind = 'cats';
+					break;
+				case 'Собаки': 
+					kind = 'dogs';
+					break;
+				default: 
+					kind = '';
+			}
+			return require(`../assets/images/${kind}/${id}.jpg`)
+		}
+	},
+	computed: {
+		filteredPets() {
+			return this.pets.filter(pet => {
+				if (pet.kind === this.kindOfPets) return true;
+				if (pet.breed === this.breedOfPets) return true;
+				return false;
+			})
+		}
+	},
+	async created() {
+		try {
+			this.pets = await PetsImport.getPets()
+		} catch (error) {
+			this.error = error.message
+		}
 	}
-  },
-
-  async created() {
-    try {
-      this.pets = await PetsImport.getPets()
-	} catch (error) {
-      this.error = error.message
-	}
-  }
 }
 </script>
-
-<style scoped>
-@import '../assets/css/style.css';
-@import '../assets/css/reset.css';
-</style>
--- client/src/components/homeTab.vue
+++ client/src/components/homeTab.vue
@@ -1,5 +1,11 @@
 <template>
 	<div class="layout__row layout__row_body">
+		<div class="layout__row_catalog">
+			<div class="container box">
+				<a class="catalog-a" @click="catalogPressed('Кошки', '')" href="#">Кошки</a>
+				<a class="catalog-a" @click="catalogPressed('Собаки', '')" href="#">Собаки</a>
+			</div>
+		</div>
 		<video class="main-video" autoplay loop muted playsinline> 
 			<source src="../assets/video/main-cat.mp4" type="video/mp4"> 
 			<source src="../assets/video/main-cat.webm" type="video/webm"> 
@@ -8,10 +14,12 @@
 </template>
 
 <script>
-
+export default {
+	name: 'homeTab',
+	methods: {
+		catalogPressed(kind, breed) {
+			this.$emit('catalogPressed', {kind, breed})
+		}
+	}
+}
 </script>
-
-<style scoped>
-@import '../assets/css/style.css';
-@import '../assets/css/reset.css';
-</style>
