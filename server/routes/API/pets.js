8070bf0f722a83fde62bd518864839a10f1e5d6c
vant110 <vant110@yandex.ru>
2021-05-22 03:47:57 +0400

    Add sort

--- server/routes/API/pets.js
+++ server/routes/API/pets.js
@@ -5,16 +5,15 @@ const router = express.Router()
 
 router.get('/', async (request, response) => {
   const pets = await loadPetsCollection()
-  response.send(await pets.find({}).toArray())
+  response.send(await pets.find({}).sort({name:1}).toArray())
 })
 
 async function loadPetsCollection () {
-  const uri = 'mongodb+srv://UserPets:UserPets@petscluster.2j5zm.mongodb.net/pets.pets?retryWrites=true&w=majority'
+  const uri = 'mongodb+srv://UserPets:UserPets@petscluster.2j5zm.mongodb.net/pets?retryWrites=true&w=majority'
   const client = await mongodb.MongoClient.connect(uri, {
     useUnifiedTopology: true,
     useNewUrlParser: true
   })
-
   return client.db('pets').collection('pets')
 }
 
