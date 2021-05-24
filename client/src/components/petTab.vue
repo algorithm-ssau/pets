2f3547a85e058fe60227357c4a378413fb7474b3
arseniy6666 <arseniy-666@mail.ru>
2021-05-24 13:18:17 +0400

    test

--- client/src/components/petTab.vue
+++ client/src/components/petTab.vue
@@ -1,32 +1,49 @@
 <template>
-<div class="layout__row layout__row_body">
-	<div class="container card">
-	<!--TODO обработчик-->
-		<a class="card-a-with-img" href="">
-			<img :src="require(`../assets/images/${petInfo.imgPath}`)">
-		</a>
-		<div class="card-info">			
-			<span class="span-block card-info-name">{{petInfo.name}}</span>		
-			<div class="box-vert">
-				<div class="box">
-					<span class="span-block card-info-left-span">Порода:</span>
-					<span class="span-block">{{ petInfo.breed }}</span>
-				</div>
-				<div class="box">
-					<span class="span-block card-info-left-span">Владелец:</span>
-					<span class="span-block">{{ petInfo.owner }}</span>
+	<div class="layout__row layout__row_body">
+		<div class="container card">
+			<a class="card-a-with-img" href="">
+				<img :src="getImgPath(petInfo._id)">
+			</a>
+			<div class="card-info">			
+				<span class="span-block card-info-name">{{ petInfo.name }}</span>		
+				<div class="box-vert">
+					<div class="box">
+						<span class="span-block card-info-left-span">Порода:</span>
+						<span class="span-block">{{ petInfo.breed }}</span>
+					</div>
+					<div class="box">
+						<span class="span-block card-info-left-span">Владелец:</span>
+						<span class="span-block">{{ petInfo.owner }}</span>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </template>
 
 <script>
-
 export default {
 	name: 'petTab',
-    props: ['petInfo']
+	props: {
+		petInfo: {},
+		kindOfPets: String,
+		breedOfPets: String
+	},
+	methods: {
+		getImgPath(id) {
+			var kind
+			switch (this.kindOfPets) {
+				case 'Кошки': 
+					kind = 'cats';
+					break;
+				case 'Собаки': 
+					kind = 'dogs';
+					break;
+				default: 
+					kind = '';
+			}
+			return require(`../assets/images/${kind}/${id}.jpg`)
+		}
+	}
 }
-
 </script>
