a5ff645f8fb3be12fd24aa8722e6ebdadc75f887
Ildar <ahmadiev-1234@mail.ru>
2021-05-24 20:23:31 +0400

    Refactor vue

---
+++ client/src/components/components/catalogTab.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="layout__row layout__row_body">
+        <div class="container box"
+			v-if="findedPets.length === 0"
+		>
+			<span>Ничего не найдено</span>
+		</div>
+		<div class="container"
+			v-else
+		>
+			<div class="grid" v-cloak>
+				<div class="pet"
+				v-for="(pet, index) in findedPets"
+				:item="pet"
+				:index="index"
+				:key="pet._id"				
+				>
+					<a class="cards-a-with-img" @click="cardPressed(pet)" href="#">
+						<img :src="getImgPath(pet)">
+					</a>
+					<div class="box cards-div-name">
+						<a class="cards-name" @click="cardPressed(pet)" href="#">{{ pet.name }}</a>
+					</div>
+				</div>
+				<p class="error" v-if="error">{{ error }}</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'catalogTab',
+	props: {
+		findedPets: []
+	},
+	methods: {
+		cardPressed(pet) {
+			this.$emit('cardPressed', pet)
+		},
+		getImgPath(pet) {
+			var kind
+			switch (pet.kind) {
+				case 'Кошки': 
+					kind = 'cats';
+					break;
+				case 'Собаки': 
+					kind = 'dogs';
+					break;
+				case 'Попугаи': 
+					kind = 'parrots';
+					break;
+				default: 
+					kind = '';
+			}
+			return require(`../assets/images/${kind}/${pet._id}.jpg`)
+		}
+	}
+}
+</script>
