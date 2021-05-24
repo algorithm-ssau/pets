c38c7df9815b7073286332ea3548815c38b13365
skipah7 <80385601+skipah7@users.noreply.github.com>
2021-05-24 21:44:19 +0400

    Standard review

--- client/src/api/PetsImport.js
+++ client/src/api/PetsImport.js
@@ -5,16 +5,13 @@ const url = 'http://localhost:5000/API/pets'
 class PetsImport {
   // get pets method
   static getPets () {
-    return new Promise((resolve, reject) => 
-    {
-      axios.get(url).then((response) => 
-      {
+    return new Promise((resolve, reject) => {
+      axios.get(url).then((response) => {
         const data = response.data
         resolve(
           data.map(pets => ({
             ...pets,
-          }))
-        )
+          })))
       })
       .catch((error) => {
         reject(error)
