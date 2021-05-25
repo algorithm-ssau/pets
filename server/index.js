bab77c141889d478f8bdabecfd2acb4b06bb4bbd
skipah7 <80385601+skipah7@users.noreply.github.com>
2021-05-25 16:42:18 +0400

    Production handle

--- server/index.js
+++ server/index.js
@@ -8,4 +8,10 @@ const app = express()
 app.use(cors())
 app.use('/API/pets', pets)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/public/'))
+
+  app.get(/.*/, (request, response) => response.sendFile(__dirname + '/public/index.html'))
+}
+
 app.listen(PORT, () => console.log(`Server has been started on port ${PORT}...`))
